2c29f2bf91875c9613463979bd6515cf2ea7f7eb
Esteban Indiveri <esteban.indiveri@gmail.com>
2021-05-01 17:56:58 -0300

    fetch apimovies get

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 
 import {View, Text} from 'react-native';
+import { getNewsMoviesApi } from '../api/movies';
 
 export default function Home() {
+  const [newMovies, setNewMovies] = useState(null);
+  const getMovies = async()=>{
+    const data = await getNewsMoviesApi();
+    setNewMovies(data);
+  };
+  useEffect(()=>{
+    getMovies();
+  },[]);
+  console.log(newMovies);
+
   return (
     <View>
       <Text>Estamos en Home</Text>
--- utils/constans.js
+++ utils/constans.js
@@ -1,1 +1,6 @@
-export const API_KEY='95fe8dea25d9ca393d8f89c52f789fe2';
+export const API_HOST = 'https://api.themoviedb.org/3';
+
+export const API_KEY = '95fe8dea25d9ca393d8f89c52f789fe2';
+
+export const LANG = 'es-ES';
+
