9fe899a097d84b702b4b2babad446c444af14dd8
Esteban Indiveri <esteban.indiveri@gmail.com>
2021-05-01 18:32:10 -0300

    styles carousel

--- src/components/CarouselVertical.js
+++ src/components/CarouselVertical.js
@@ -1,27 +1,60 @@
-import React,{useState,useEffect} from 'react';
-import { StyleSheet,View,Image,Dimensions,TouchableWithoutFeedback,Text} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {
+  StyleSheet,
+  View,
+  Image,
+  Dimensions,
+  TouchableWithoutFeedback,
+} from 'react-native';
+import {Text, Title} from 'react-native-paper';
 import Carousel from 'react-native-snap-carousel';
+import {BASE_PATH_IMG} from '../../utils/constans';
 const {width} = Dimensions.get('window');
-const ITEM_WIDTH=Math.round(width*0.7);
-export default function CarouselVertical(props){
-    const {data} = props;
-    console.log(data);
-    return (
-        <Carousel
-        layout={'default'}
-        data={data}
-        renderItem={(item)=><RenderItem data={item}/>}
-        sliderWidth={width}
-        itemWidth={ITEM_WIDTH}
-        />
-    );
+const ITEM_WIDTH = Math.round(width * 0.7);
+export default function CarouselVertical(props) {
+  const {data} = props;
+  // console.log(data);
+  return (
+    <Carousel
+      layout={'default'}
+      data={data}
+      renderItem={item => <RenderItem data={item} />}
+      sliderWidth={width}
+      itemWidth={ITEM_WIDTH}
+    />
+  );
 }
-function RenderItem(props){
-    const {item} = props;
-    return(
-        <View>
-            <Text>Pelicula</Text>
-        </View>
-    )
+function RenderItem(props) {
+  const {data} = props;
+  const {title, poster_path} = data.item;
+  const imageUrl = `${BASE_PATH_IMG}/w500/${poster_path}`;
+  return (
+    <TouchableWithoutFeedback onPress={() => console.log('hola')}>
+      <View style={styles.card}>
+        <Image style={styles.image} source={{uri: imageUrl}} />
+        <Title style={styles.title}>{title}</Title>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+}
 
-}
+const styles = StyleSheet.create({
+  card: {
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 10,
+    },
+    shadowOpacity: 1,
+    shadowRadius: 10,
+  },
+  image: {
+    width: '100%',
+    height: 450,
+    borderRadius: 20,
+  },
+  title: {
+    marginHorizontal: 10,
+    marginTop: 10,
+  },
+});
--- utils/constans.js
+++ utils/constans.js
@@ -4,3 +4,4 @@ export const API_KEY = '95fe8dea25d9ca393d8f89c52f789fe2';
 
 export const LANG = 'es-ES';
 
+export const BASE_PATH_IMG = 'https://image.tmdb.org/t/p';
