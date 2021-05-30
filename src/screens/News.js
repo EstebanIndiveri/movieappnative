bca7947b14a17cc6d623516918c0682c41619731
Esteban Indiveri <esteban.indiveri@gmail.com>
2021-05-30 18:33:42 -0300

    added navigation btn, loadmore btn

--- src/screens/News.js
+++ src/screens/News.js
@@ -11,35 +11,61 @@ import {Button, Text} from 'react-native-paper';
 import {getNewsMoviesApi} from '../api/movies';
 import {map} from 'lodash';
 import {BASE_PATH_IMG} from '../../utils/constans';
-
+import usePreferences from '../hooks/usePreferences'
 const {width} = Dimensions.get('window');
 
 export default function NewMovie(props) {
   const {navigation} = props;
   const [movies, setMovies] = useState(null);
   const [page, setPage] = useState(1);
+  const [showBtnMore, setShowBtnMore] = useState(true);
+  const {theme} =usePreferences();
   useEffect(() => {
     getNewsMoviesApi(page).then(res => {
-      setMovies(res.results);
+      const totalPages=res.total_pages;
+      if(page<totalPages){
+        if(!movies){
+          setMovies(res.results);
+        }else{
+          setMovies([...movies,...res.results]);
+        }
+      }else{
+        setShowBtnMore(false);
+      }
     });
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [page]);
   return (
     <ScrollView>
       <View style={styles.container}>
         {map(movies, (movie, index) => (
-          <Movie key={index} movie={movie} />
+          <Movie key={index} movie={movie} navigation={navigation}/>
         ))}
       </View>
+      {showBtnMore&&(
+        <Button
+        mode='contained'
+        contentStyle={styles.loadMoreContainer}
+        style={styles.loadMore}
+        labelStyle={{color:theme==='dark'?'#FFF':'#000'}}
+        onPress={()=>setPage(page+1)}
+        >
+          Cargar Más...
+        </Button>
+      )}
     </ScrollView>
   );
 }
 
 function Movie(props) {
-  const {movie} = props;
+  const {movie,navigation} = props;
 
   const {title, id, poster_path} = movie;
+  const getMovie=()=>{
+    navigation.navigate('Movie',{id});
+  }
   return (
+    <TouchableWithoutFeedback onPress={getMovie}>
     <View style={styles.movie}>
       {poster_path ? (
         <Image
@@ -50,6 +76,7 @@ function Movie(props) {
         <Text>{title}</Text>
       )}
     </View>
+    </TouchableWithoutFeedback>
   );
 }
 const styles = StyleSheet.create({
@@ -68,4 +95,11 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%',
   },
+  loadMoreContainer:{
+    paddingTop:10,
+    paddingBottom:30
+  },
+  loadMore:{
+    backgroundColor:'transparent'
+  }
 });
