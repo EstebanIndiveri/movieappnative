df8de93134dc09a4844d25e8ba545071baea759d
Esteban Indiveri <esteban.indiveri@gmail.com>
2021-05-30 18:46:36 -0300

    added search screen movies

--- src/screens/Search.js
+++ src/screens/Search.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 import {View, Text} from 'react-native';
+import { searchMovieApi } from '../api/movies';
 
 export default function Search() {
+  const [movies, setMovies] = useState(null);
+
+  useEffect(()=>{
+    searchMovieApi('spiderman').then((response)=>{
+      setMovies(response.results);
+    });
+  },[]);
+
   return (
     <View>
       <Text>Estamos en Search</Text>
